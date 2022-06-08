7d2fd456dd6b69014f185e7c8a264e359262c6af
Rachel Howell <rac777blue@hotmail.com>
2022-06-08 17:52:19 +0100

    import fonts

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react"
-import Button from "../components/Button"
+import Button from "../../components/Button"
 import { Link } from "react-router-dom"
+import "./LandingPage.css"
 
 function LandingPage() {
     return (
