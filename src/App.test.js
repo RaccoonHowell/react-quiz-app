f8a1e75565631878553792b409b75f37782ae6be
Rachel Howell <rac777blue@hotmail.com>
2022-06-20 12:10:24 +0100

    add first test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react"
+import App from "./App"
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+jest.mock("nanoid", () => {
+    return { nanoid: () => "1234" }
+});
+
+describe("App component", () => {
+    it("renders landing page", () => {
+        render(<App />)
+
+        const h1Element = screen.getByText(/quizzical/i)
+        const h2Element = screen.getByText(/random multiple choice questions/i)
+        const buttonElement = screen.getByText(/start quiz/i)
+
+        expect(h1Element).toBeInTheDocument()
+        expect(h2Element).toBeInTheDocument()
+        expect(buttonElement).toBeInTheDocument()
+    })
+})
