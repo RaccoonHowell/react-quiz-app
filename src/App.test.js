311978d067bb184390a543f9b38fab5cef67945c
Rachel Howell <rac777blue@hotmail.com>
2022-06-20 16:10:54 +0100

    add quizpage tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from "./App"
 
 jest.mock("nanoid", () => {
     return { nanoid: () => "1234" }
-});
+})
 
 describe("App component", () => {
     it("renders landing page", () => {
@@ -21,9 +21,9 @@ describe("App component", () => {
     it("changes to check answers button when landing page start quiz button is clicked", () => {
         render(<App />)
 
-        const startquizButton = screen.getByText(/start quiz/i)
+        const startQuizButton = screen.getByText(/start quiz/i)
                
-        fireEvent.click(startquizButton)
+        fireEvent.click(startQuizButton)
 
         expect(screen.getByText(/check answers/i)).toBeInTheDocument()
     })
---
+++ src/__tests__/QuizPage.test.js
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import QuizPage from "../pages/QuizPage"
+
+jest.mock("nanoid", () => {
+    return { nanoid: () => "1234" }
+})
+
+describe("QuizPage component", () => {
+    it("renders play again button", () => {
+        render(<QuizPage />)
+
+        const buttonElement = screen.getByText(/check answers/i)
+
+        expect(buttonElement).toBeInTheDocument()
+    })
+
+    it("changes to play again button when check answers button is clicked", () => {
+        render(<QuizPage />)
+
+        const checkAnswersButton = screen.getByText(/check answers/i)
+               
+        fireEvent.click(checkAnswersButton)
+
+        expect(screen.getByText(/play again/i)).toBeInTheDocument()
+    })
+})
