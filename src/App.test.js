afb5f19023db0eee4bafa9111a2cd137619d6190
Rachel Howell <rac777blue@hotmail.com>
2022-06-20 12:35:29 +0100

    add test to check landing page button changes when clicked

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,7 @@ jest.mock("nanoid", () => {
 describe("App component", () => {
     it("renders landing page", () => {
         render(<App />)
-
+        
         const h1Element = screen.getByText(/quizzical/i)
         const h2Element = screen.getByText(/random multiple choice questions/i)
         const buttonElement = screen.getByText(/start quiz/i)
@@ -16,5 +16,15 @@ describe("App component", () => {
         expect(h1Element).toBeInTheDocument()
         expect(h2Element).toBeInTheDocument()
         expect(buttonElement).toBeInTheDocument()
+    })
+
+    it("changes to check answers button when landing page start quiz button is clicked", () => {
+        render(<App />)
+
+        const startquizButton = screen.getByText(/start quiz/i)
+               
+        fireEvent.click(startquizButton)
+
+        expect(screen.getByText(/check answers/i)).toBeInTheDocument()
     })
 })
